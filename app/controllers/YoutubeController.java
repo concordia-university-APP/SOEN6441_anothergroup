f9b589b436476bb0938429bcb88d6aa46a4b218c
yehiametwally55 <yehiametwally55@gmail.com>
2024-11-02 16:21:47 -0400

    fixed multiple search , fixed channel page hyperlink not being clickable

--- app/controllers/YoutubeController.java
+++ app/controllers/YoutubeController.java
@@ -1,35 +1,42 @@
 package controllers;
 
+import scala.Option;
+import scala.compat.java8.OptionConverters;
 import com.google.api.services.youtube.model.SearchResult;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.YoutubeService;
 import views.html.search;
-import scala.jdk.javaapi.OptionConverters;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import scala.Option;
+
 
 public class YoutubeController extends Controller {
 
+    private static List<SearchResult> combinedResults = new ArrayList<>();
+
     public Result search(String query) {
         try {
-            List<SearchResult> results = YoutubeService.searchResults(query);
-            Option<List<SearchResult>> scalaResults;
-            if (results.isEmpty()) {
-                scalaResults = Option.empty();
-            } else {
-                scalaResults = OptionConverters.toScala(Optional.of(results));
-            }
-            return ok(search.render(scalaResults));
+            // Fetch new search results
+            List<SearchResult> newResults = YoutubeService.searchResults(query);
+
+            // Add new results to the combined list
+            combinedResults.addAll(newResults);
+
+            // Convert combined results to Scala Option for rendering
+            Option<List<SearchResult>> scalaResults = OptionConverters.toScala(Optional.of(combinedResults));
+            return ok(views.html.search.render(scalaResults));
+
         } catch (IOException e) {
             return internalServerError("Error occurred while searching YouTube: " + e.getMessage());
         } catch (GeneralSecurityException e) {
             throw new RuntimeException(e);
         }
     }
+
     public Result searchForm() {
         Optional<List<SearchResult>> results = Optional.empty();
         return ok(search.render(OptionConverters.toScala(results)));
